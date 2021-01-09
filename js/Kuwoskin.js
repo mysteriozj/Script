9ba23c9766f027a7ab6588e34eb70057af5c3923
Piggy <mysteriozj@163.com>
2021-01-10 05:47:37 +0800

    skin

--- js/Kuwoskin.js
+++ js/Kuwoskin.js
@@ -1,6 +1,6 @@
 /*
 酷我皮肤 转自nzw9314
-^https?:\/\/vip1\.kuwo\.cn\/(vip\/v2\/theme)  url script-response-body   https://raw.githubusercontent.com/mysteriozj/Script/master/js/Kuwoskin.js
+^https?:\/\/vip1\.kuwo\.cn\/(vip\/v2\/theme) url script-response-body https://raw.githubusercontent.com/mysteriozj/Script/master/js/Kuwoskin.js
 */
 
 var obj = JSON.parse($response.body);
