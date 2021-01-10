f6a01c73dd182b7a1fdcfaa90d4acd2952014f7b
Piggy <mysteriozj@163.com>
2021-01-10 16:27:46 +0800

    Crack

--- js/Tanhua.js
+++ js/Tanhua.js
@@ -8,7 +8,7 @@ var body = $response.body;
 var obj = JSON.parse(body);
 
 obj.data.is_vip = 1;
-obj.data.user_viptime = 2033;
+obj.data.user_viptime = 2033-10-10;
 
 body = JSON.stringify(obj);
 $done({body});
