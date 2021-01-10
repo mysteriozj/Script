d15257eecf9c501a6bb44d173bd1d3ec1253f2a3
Piggy <mysteriozj@163.com>
2021-01-10 16:17:52 +0800

    Crack

---
+++ js/Tanhua.js
@@ -0,0 +1,14 @@
+/*
+Tanhua
+^https:\/\/1008610010\.laikanshu\.top\/Member\/getUserInfo
+hostname = 1008610010.laikanshu.top 
+*/
+
+var body = $response.body; 
+var obj = JSON.parse(body);
+
+obj.data.is_vip = 1;
+obj.data.user_viptime = 2099-01-10;
+
+body = JSON.stringify(obj);
+$done({body});
