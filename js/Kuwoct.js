b0a80655f249a814372ea75655e0b93807e5a268
Piggy <mysteriozj@163.com>
2021-01-11 21:53:40 +0800

    Crack

--- js/Kuwoct.js
+++ js/Kuwoct.js
@@ -1,6 +1,7 @@
 /*
 酷我听书
-^https:\/\/audiobookpay\.kuwo\.cn\/a\.p*
+^https:\/\/audiobookpay\.kuwo\.cn\/a\.p
+^http:\/\/tingshu\.kuwo\.cn\/v2\/api\/user\/info
 
 hostname = audiobookpay.kuwo.cn
 */
@@ -10,6 +11,7 @@ var url = $request.url;
 var obj = JSON.parse(body);
 
 const vip = '/a.p';
+const vip1 = '/v2/api/user/info';
 
 if (url.indexOf(vip) != -1) {
     obj.packs.end = 4071731352;
@@ -17,6 +19,11 @@ if (url.indexOf(vip) != -1) {
 	obj.packs.type = 2;
 	obj.packs.period = 111;
 	obj.packs.bought_vip_end = 4071731352;
+	body = JSON.stringify(obj);
+ }
+if (url.indexOf(vip) != -1) {
+    obj.data.vipExpires = 4071731352;
+	obj.data.vipType = 1;
 	body = JSON.stringify(obj);
  }
  
