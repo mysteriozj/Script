5037267e508ac2c31734f94cca384cb0df451ed2
Piggy <mysteriozj@163.com>
2021-01-11 20:35:42 +0800

    Crack

--- js/Kuwoct.js
+++ js/Kuwoct.js
@@ -6,25 +6,5 @@
 hostname = tingshu.kuwo.cn,audiobookpay.kuwo.cn
 */
 
-var body = $response.body;
-var url = $request.url;
-var obj = JSON.parse(body);
-
-const info = '/api/user/info*';
-
-const vip = '/a.p*';
-
-if (url.indexOf(info) != -1) {
-	obj.data.vipExpires = 4070952349;
-	obj.data.vipType = 1;
-	body = JSON.stringify(obj);
- }
- if (url.indexOf(vip) != -1) {
-	obj["limitfree"] = 1;
-	obj["downright"] = 1;
-	obj["playright"] = 1;
-	obj["policytype"] = 1;
-	obj["pay"] = "0";
-	body = JSON.stringify(obj);
- }
+body = $response.body.replace(/vipExpires":\d/g, 'vipExpires":4071731352').replace(/vipType":0/g, 'vipType":1').replace(/limitfree":\d/g, 'limitfree":1').replace(/policytype":\d/g, 'policytype":1').replace(/downright":\d/g, 'downright":1').replace(/playright":\d/g, 'playright":1');
 $done({body});
