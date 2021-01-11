54ab95f323dd0ef0600080113749cef288b0e79b
Piggy <mysteriozj@163.com>
2021-01-11 21:34:06 +0800

    Crack

--- js/Kuwoct.js
+++ js/Kuwoct.js
@@ -2,23 +2,23 @@
 酷我听书
 ^http:\/\/tingshu\.kuwo\.cn\/v2\/api\/user\/info*
 ^https:\/\/audiobookpay\.kuwo\.cn\/a\.p
+^https:\/\/audiobookpay\.kuwo\.cn\/a\.p\?*
 
 hostname = tingshu.kuwo.cn,audiobookpay.kuwo.cn
 */
-/*
-body = $response.body.replace(/vipExpires":\d/g, 'vipExpires":4071731352').replace(/vipType":0/g, 'vipType":1').replace(/limitfree":\d/g, 'limitfree":1').replace(/policytype":\d/g, 'policytype":1').replace(/downright":\d/g, 'downright":1').replace(/playright":\d/g, 'playright":1');
-$done({body});
-*/
+
 var body = $response.body;
 var url = $request.url;
 var obj = JSON.parse(body);
 
-const info = '/user/info*';
+const info = '/a.p*';
 const vip = '/a.p';
 
 if (url.indexOf(info) != -1) {
-	obj["vipExpires"] = "4071731352";
-	obj["vipType"] = "1";
+	obj["bought_vip_end"] = "4071731352";
+	obj["type"] = "2";
+	obj["bought_vip"] = "1";
+	obj["end"] = "4071731352";
 	body = JSON.stringify(obj);
  }
 if (url.indexOf(vip) != -1) {
