41286555d9477bb00850943036d72a3ce844df10
Piggy <mysteriozj@163.com>
2021-01-11 21:47:48 +0800

    Crack

--- js/Kuwoct.js
+++ js/Kuwoct.js
@@ -9,7 +9,7 @@ var body = $response.body;
 var url = $request.url;
 var obj = JSON.parse(body);
 
-const vip = '/a.p*';
+const vip = '/a.p';
 
 if (url.indexOf(vip) != -1) {
     obj.packs.end = 4071731352;
