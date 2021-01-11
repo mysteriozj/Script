7c3152ed4fdd6cf0006dfff8834f0688672c16c3
Piggy <mysteriozj@163.com>
2021-01-12 05:28:10 +0800

    Crack

--- js/Yangqi.js
+++ js/Yangqi.js
@@ -1,27 +1,16 @@
 /*
 氧气
-^https:\/\/api.o2ting.com\/api\/V2\/(user\/GetUserInfo|audio\/GetAudioListByIndex)\.json
+^https:\/\/api.o2ting.com\/api\/V2\/user\/GetUserInfo\.json
 
 hostname = api.o2ting.com
 */
 
 var body = $response.body;
-var url = $request.url;
 var obj = JSON.parse(body);
 
-const vip = '/api/V2/user/GetUserInfo.json';
+obj.userInfo.isMember = true;
+obj.userInfo.startDate = 2020-09-17 12:30:45;
+obj.userInfo.endDate = 2099-09-17 12:19:19;
 
-const check = '/api/V2/audio/GetAudioListByIndex.json';
-
-if (url.indexOf(vip) != -1) {
-	obj["isMember"] = "true";
-	obj["startDate"] = "2020-09-17 12:30:45";
-	obj["surplusCount"] = "2099-09-17 12:19:19";
-	body = JSON.stringify(obj);
- }
-if (url.indexOf(check) != -1) {
-	obj["isFree"] = "true";
-	body = JSON.stringify(obj);
- }
-
+body = JSON.stringify(obj);
 $done({body});
