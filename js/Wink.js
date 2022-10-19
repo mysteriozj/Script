d6009d6f3c42549ca9feec3b083793cf8651cc24
Piggy <mysteriozj@163.com>
2022-10-20 00:14:07 +0800

    .

--- js/Wink.js
+++ js/Wink.js
@@ -7,6 +7,6 @@ hostname = api-sub.meitu.com
 */
 var obj=JSON['parse']($response['body']);
 
-obj['data']={"account_type":1,"account_id":1700603851,"is_vip":true,"valid_time":1988963877000,"invalid_time":1988963877000,"derive_type":1,"derive_type_name":"超级会员","membership":{"id":4,"display_name":"wink会员","level":1,"level_name":"普通"}};
+obj.data.is_vip = true;
 
 $done({body:JSON['stringify'](obj)});
