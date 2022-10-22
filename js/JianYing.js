0f4938a9ccf22782f9253e15d8f6a33c52acffff
Piggy <mysteriozj@163.com>
2022-10-22 23:17:37 +0800

    .

---
+++ js/JianYing.js
@@ -0,0 +1,17 @@
+/*
+简影
+^http:\/\/api\.jianmovie\.com\/(cardinfo|expense)
+[MITM]
+hostname = api.jianmovie.com
+*/
+
+var body = $response.body; 
+var obj = JSON.parse(body);
+
+obj.success = 1;
+obj.is_vip = 1;
+obj.vip_type = 1;
+obj.vip_expire_time = 4071743248;
+
+body = JSON.stringify(obj);
+$done({body});
