4e2d4bb9a0aa18b7cc75c0307dd29cc367e026ce
Piggy <mysteriozj@163.com>
2022-10-22 22:14:12 +0800

    .

---
+++ js/JiMei.js
@@ -0,0 +1,19 @@
+/*
+集美
+^https:\/\/026965671aa6704b8cfc\.myminapp\.com\/hserve\/v\2\.0\/user\/info\/492072433804665\/
+^https:\/\/*\.myminapp\.com\/hserve\/v\2\.0\/user\/info\/*\/
+[MITM]
+hostname = *.myminapp.com
+*/
+
+var body = $response.body; 
+var obj = JSON.parse(body);
+
+obj.is_vip = true;
+obj.clarity_boost_max = 1000;
+obj.is_authorized = true;
+obj.is_admin = true;
+obj.download_count = 0;
+
+body = JSON.stringify(obj);
+$done({body});
